ceebf8e08d59083564a3697167e405bf3fd3fde4
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2018-01-25 10:07:52 +0300

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'vuex-loading.min.js',
+    filename: 'vuex-loading.js',
     library: 'VuexLoading',
     libraryTarget: 'umd',
     umdNamedDefine: true
